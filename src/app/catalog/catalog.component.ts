82b2050792ad29dfd3505cdbd1617ad57e8fc541
ttewabe <tomtadege@gmail.com>
2025-03-11 00:48:40 -0500

    used separate data file and added robot torso in the market

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,6 +1,6 @@
-// catalog.component.ts
 import { Component } from '@angular/core';
 import { IProduct } from './product.module';
+import { PRODUCTS } from './product.data';
 
 @Component({
   selector: 'app-catalog',
@@ -8,73 +8,12 @@ import { IProduct } from './product.module';
   styleUrls: ['./catalog.component.scss'],
 })
 export class CatalogComponent {
-  products: any;
+  products: IProduct[] = PRODUCTS;
   filter: string = '';
 
-  constructor() {
-    this.products = [
-      {
-        id: 1,
-        name: 'Advanced Robot Head',
-        description: 'High-tech robotic head with sensors',
-        price: 1200.0,
-        imageName: 'assets/images/robot-head.jpeg',
-        catagory: 'Head',
-        discount: 0.1,
-      },
-      {
-        id: 2,
-        name: 'Friendly Robot Head',
-        description: 'Friend Robot head with two eyes and a mouth',
-        price: 945.0,
-        imageName: 'assets/images/robot-head1.jpeg',
-        catagory: 'Head',
-        discount: 0,
-      },
-      {
-        id: 3,
-        name: 'Strong Robot Legs',
-        description: 'Powerful legs with high mobility',
-        price: 1050.0,
-        imageName: 'assets/images/robot-leg.jpeg',
-        catagory: 'Legs',
-        discount: 0.15,
-      },
-      {
-        id: 4,
-        name: 'Friendly Robot Legs',
-        description: 'Friend Robot legs with two eyes and a mouth',
-        price: 980.0,
-        imageName: 'assets/images/robot-leg1.jpeg',
-        catagory: 'Legs',
-        discount: 0,
-      },
-      {
-        id: 5,
-        name: 'Advanced Robot Arms',
-        description: 'High-tech robotic arms with precision control',
-        price: 1100.0,
-        imageName: 'assets/images/robot-arm.jpeg',
-        catagory: 'Arms',
-        discount: 0.12,
-      },
-      {
-        id: 6,
-        name: 'Friendly Robot Arm',
-        description: 'Friend Robot arm with two eyes and a mouth',
-        price: 1023.0,
-        imageName: 'assets/images/robot-arm1.jpeg',
-        catagory: 'Arms',
-        discount: 0,
-      },
-    ];
-  }
-
-  getFilteredProducts() {
+  getFilteredProducts(): IProduct[] {
     return this.filter === '' || this.filter === 'All'
       ? this.products
-      : this.products.filter(
-          (product: any) => product.catagory === this.filter,
-        );
+      : this.products.filter((product) => product.catagory === this.filter);
   }
 }
